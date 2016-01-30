ef2d385dadda1c7e80af0eadbf3f79eb4d9df854
Nacho <nacho@air>
2016-01-30 18:28:54 +0100

    Clean to_dataframe function and add raw option.

--- R/query.R
+++ R/query.R
@@ -68,34 +68,35 @@ buildQueryConditions <- function(conditionList=NULL) {
   return(res)
 }
 
-to_dataframe <- function(response, keep_metadata = FALSE){
-  responseNA <- lapply(response, nullToNA)
-  #responseNA[[1]]
-  #other option
-  #if values are string/numeric...
-  #dat <- do.call(rbind, lapply(dataQ1, rbind)) #this option do not suport nested lists
-  #dat <- nullToNA(dat)
-  
-  dat <- unlist(responseNA)
-  #head(dat,10)
-  nvariales <- length(unlist(responseNA[[1]]))
-  ncases <- length(responseNA)
-  datamatrix <- matrix(data = dat, nrow = ncases, ncol = nvariales, byrow = TRUE) 
-  colnames(datamatrix) <- names(unlist(responseNA[[1]]))
-  datamatrix <- as.data.frame(datamatrix)
-  head(datamatrix)
-  
-  #remove "_links." etc...
-  #optional "_", pero NO "_id"
-  idnum <- grep("_id", colnames(datamatrix), fixed = TRUE)
-  links <- grep("_links.", colnames(datamatrix), fixed = TRUE)
-  optionalsid <- grep("_", colnames(datamatrix), fixed = TRUE)
-  optionals <- optionalsid[-which(optionalsid == idnum)]
-  raw_links <- datamatrix
-  raw <- datamatrix[,-links]
-  clean <- datamatrix[,-optionals]
-  return(clean)
-  #implment option raw
+to_dataframe <- function(response, keep_metadata = "clean"){
+  if(!keep_metadata %in% c("clean", "raw", "raw_links")){
+    message("keep_metadata has to be clean, raw or raw_links")
+  }else{
+    responseNA <- lapply(response, nullToNA)
+    dat <- unlist(responseNA)
+    nvariales <- length(unlist(responseNA[[1]]))
+    ncases <- length(responseNA)
+    datamatrix <- matrix(data = dat, nrow = ncases, ncol = nvariales, byrow = TRUE) 
+    colnames(datamatrix) <- names(unlist(responseNA[[1]]))
+    datamatrix <- as.data.frame(datamatrix)
+    head(datamatrix)
+    idnum <- grep("_id", colnames(datamatrix), fixed = TRUE)
+    links <- grep("_links.", colnames(datamatrix), fixed = TRUE)
+    optionalsid <- grep("_", colnames(datamatrix), fixed = TRUE)
+    optionals <- optionalsid[-which(optionalsid == idnum)]
+    if(keep_metadata == "clean"){
+      clean <- datamatrix[,-optionals]
+      return(clean)
+    }
+    if(keep_metadata == "raw"){
+      raw <- datamatrix[,-links]
+      return(raw)
+    }
+    if(keep_metadata == "raw_links"){
+      raw_links <- datamatrix
+      return(raw_links)
+    }
+  }
 }
 
 connect <- function(url, user, pass) {
