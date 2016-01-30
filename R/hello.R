bccae2a650aed9deee42aace880d95ffbf46c21a
Nacho <nacho@air>
2016-01-30 12:32:19 +0100

    Test crear paquete

--- R/hello.R
+++ R/hello.R
@@ -1,17 +1,30 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' lo que hace la funcion
+#' 
+#' en detalle
+#' 
+#' @param parametro 1 \code{}
+#' 
+#' @param param 2
+#' 
+#' @return Ea0 returns a data.frame of class renyi with the selected indices. 
+#' @return EinvD returns a vector with EinvD. 
+#' @return EJ returns a a vector with Pielou's index. 
+#' @return Evar returns a vector with Evar. 
+#'
+#'
+#' @export
+#' 
+#' @examples 
+#' Ea0(A = dummy$abun) 
+#' Ea0(A = dummy$abun, scales = c(0.25,0.5,1,2,4,8,Inf)) 
+#' EinvD(A = dummy$abun)
+#' EJ(A = dummy$abun)
+#' Evar(A = dummy$abun)
+#' #calculate all of them
+#' eve1 <- Eve(A = dummy$abun)
+#' eve2 <- Eve(A = dummy$abun, scales = c(0.25,0.5,1,2,4,8,Inf))
+#' pairs(eve1)
+#' pairs(eve2)
 
 hello <- function() {
   print("Hello, world!")
