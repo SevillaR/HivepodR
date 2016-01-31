3fffb74d1e9904f4aa1d2961e19b088037f320ae
pjmolina <pjmolina@gmail.com>
2016-01-31 20:03:59 +0100

    Added documentation for connect() and resource()

--- R/connect.R
+++ R/connect.R
@@ -1,8 +1,22 @@
+#' Setups a connection.
+#' 
+#' Setup a connection to a given Hivepod backend with credentials.
+#' 
+#' @param url The base URL to connect to.
+#' @param user The user credential.
+#' @param pass The password for the user.
+#' 
+#' @return connect returns a connection object for a Hivepod backend. 
+#' This connection object can be used to discover and access to resources. 
+#' @export
+#' @examples 
+#' cnx <- connect("http://jacaton-r.herokuapp.com", "demo", "1234") 
+
 connect <- function(url, user, pass) {
   urlbase <- handle(url)
   rping <- GET(handle=urlbase, path="ping")
-  
   dataPing <- content(rping, type="application/json")
+
   status <- GET(handle=urlbase,  config=authenticate(user, pass), path="api/status")
   dataQ1 <- content(status, type="application/json")
   
--- R/queryRaw.R
+++ R/queryRaw.R
@@ -1,7 +1,7 @@
-queryRaw <- function(res, query) {
-  urlbase <- handle(res[[1]])
-  aut <- authenticate(res[[2]], res[[3]])
-  q1 <- GET(handle=urlbase, config=aut, path=paste("api/", res[[5]], query, sep="") )
+queryRaw <- function(resource, query) {
+  urlbase <- handle(resource[[1]])
+  aut <- authenticate(resource[[2]], resource[[3]])
+  q1 <- GET(handle=urlbase, config=aut, path=paste("api/", resource[[5]], query, sep="") )
   dataQ1 <- content(q1, type="application/json")
   df <- to_dataframe(dataQ1)
   return (df)
